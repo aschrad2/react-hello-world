1c1faef896ec6033f07b8e49f0aad4281ff2de74
Austin Schrader <austin.jschrader@gmail.com>
2020-06-20 14:04:05 -0700

    Add the React component or React component type called ShoppingList

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+class ShoppingList extends React {
+  render() {
+    return (
+      <div className='shopping-list'>
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
